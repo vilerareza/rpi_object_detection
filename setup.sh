366e13b5f0f0782fb83b9665e5acc9a8cf370c5f
Reza Vilera <vilera_reza99@yahoo.com>
2023-07-27 18:42:23 +0700

    Adding support for various detection model

--- setup.sh
+++ setup.sh
@@ -13,11 +13,11 @@ python3 -m pip install -r requirements.txt
 # Efficient Det Lite 0
 # MODEL_PATH='https://storage.googleapis.com/download.tensorflow.org/models/tflite/task_library/object_detection/rpi/lite-model_efficientdet_lite0_detection_metadata_1.tflite' 
 # Efficient Det Lite 1
-# MODEL_PATH='https://tfhub.dev/tensorflow/lite-model/efficientdet/lite1/detection/default/1?lite-format=tflite'
+MODEL_PATH='https://tfhub.dev/tensorflow/lite-model/efficientdet/lite1/detection/default/1?lite-format=tflite'
 # Efficient Det Lite 3
 # MODEL_PATH='https://tfhub.dev/tensorflow/lite-model/efficientdet/lite3/detection/default/1?lite-format=tflite'
 # Efficient Det Lite 4
-MODEL_PATH='https://tfhub.dev/tensorflow/lite-model/efficientdet/lite4/detection/default/2?lite-format=tflite'
+# MODEL_PATH='https://tfhub.dev/tensorflow/lite-model/efficientdet/lite4/detection/default/2?lite-format=tflite'
 
 # Mobilenet v2 RetinaNet
 # MODEL_PATH='https://tfhub.dev/google/lite-model/qat/mobilenet_v2_retinanet_256/1?lite-format=tflite'
