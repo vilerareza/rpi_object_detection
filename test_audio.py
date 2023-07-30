572d8273bc06533afb85b3123c176166d1b75b88
Reza Vilera <vilera_reza99@yahoo.com>
2023-07-30 14:32:36 +0700

    Adding audio features

---
+++ test_audio.py
@@ -0,0 +1,26 @@
+'''
+Program to test the basic audio function using Pygame.
+The speaker should be connected to 3.5 mm stereo jack on the RPi
+'''
+
+import time
+from pygame import mixer
+
+audio_path = 'audio_test.mp3'
+vol = 0.7
+
+# Initialize pygame mixer
+mixer.init()
+# Loading the audio
+mixer.music.load(audio_path)
+# Setting the volume
+mixer.music.set_volume(vol)
+# Play the audio file
+
+while True:
+
+    mixer.music.play()
+    query = input("Press 'q' to quit the loop.")
+    if query == 'q':
+        break
+    time.sleep(1)
