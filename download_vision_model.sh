50960eec8fd19a75770fc4acb51430635df496d9
Reza Vilera <vilera_reza99@yahoo.com>
2023-11-26 11:30:25 +0700

    updating requirements for git

--- download_vision_model.sh
+++ download_vision_model.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-  DATA_DIR="./"
+  DATA_DIR="./models"
+  mkdir ${DATA_DIR}
 else
   DATA_DIR="$1"
 fi
 
 # Install Python dependencies
 # python3 -m pip install pip --upgrade
-python3 -m pip install -r requirements.txt
+# python3 -m pip install -r requirements.txt
 
 # Efficient Det Lite 0
 # MODEL_PATH='https://storage.googleapis.com/download.tensorflow.org/models/tflite/task_library/object_detection/rpi/lite-model_efficientdet_lite0_detection_metadata_1.tflite' 
@@ -25,11 +26,11 @@ MODEL_PATH='https://tfhub.dev/tensorflow/lite-model/efficientdet/lite1/detection
 # MODEL_PATH='https://tfhub.dev/tensorflow/lite-model/ssd_mobilenet_v1/1/default/1?lite-format=tflite'
 
 # Download TF Lite models
-FILE=${DATA_DIR}/model_obj_detection.tflite
-if [ ! -f "$FILE" ]; then
-  curl \
-    -L ${MODEL_PATH} \
-    -o ${FILE}
-fi
+# FILE=${DATA_DIR}/model_obj_detection.tflite
+# if [ ! -f "$FILE" ]; then
+#   curl \
+#     -L ${MODEL_PATH} \
+#     -o ${FILE}
+# fi
 
-echo -e "Downloaded files are in ${DATA_DIR}"
+# echo -e "Downloaded files are in ${DATA_DIR}"
